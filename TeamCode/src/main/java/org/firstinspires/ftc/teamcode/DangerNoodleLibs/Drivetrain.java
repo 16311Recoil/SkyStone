cb6d6bca083ad85633ee6869907467976b1d405c
principledPanda <anish.c.pandya@gmail.com>
2019-09-09 16:40:58 -0500

    Drivetrain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DangerNoodleLibs/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DangerNoodleLibs/Drivetrain.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.DangerNoodleLibs;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class Drivetrain {
 
@@ -21,5 +22,14 @@ public class Drivetrain {
         bl = this.opMode.hardwareMap.dcMotor.get("bl");
         br = this.opMode.hardwareMap.dcMotor.get("br");
 
+        fr.setDirection(DcMotor.Direction.FORWARD);
+        br.setDirection(DcMotor.Direction.FORWARD);
+        bl.setDirection(DcMotor.Direction.FORWARD);
+        fl.setDirection(DcMotor.Direction.FORWARD);
+
+
+
+
     }
+
 }
