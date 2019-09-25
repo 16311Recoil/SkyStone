524a93de34c41aad579935cc756f43ffd95d1195
AdityaAngajala <aditya.angajala01@k12.leanderisd.org>
2019-09-25 17:24:42 -0500

    Started initiializing motors for the stacker class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DangerNoodleLibs/Stacker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DangerNoodleLibs/Stacker.java
@@ -1,6 +1,30 @@
 package org.firstinspires.ftc.teamcode.DangerNoodleLibs;
 
-public class Stacker {
+import android.graphics.Path;
 
-    public void intake(double power){}
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+public class Stacker {
+    private LinearOpMode opMode;
+    private DcMotor il; //intake left
+    private DcMotor ir; //intake right
+    private DcMotor ll; //lift left
+    private DcMotor lr; //lift right
+    private Servo armRotater;
+    private Servo pincher;
+
+    public Stacker(LinearOpMode opMode) {
+        il = this.opMode.hardwareMap.dcMotor.get("il");
+        ir = this.opMode.hardwareMap.dcMotor.get("ir");
+        ll = this.opMode.hardwareMap.dcMotor.get("ll");
+        lr = this.opMode.hardwareMap.dcMotor.get("lr");
+        armRotater = this.opMode.hardwareMap.servo.get("armRotater");
+        pincher = this.opMode.hardwareMap.servo.get("pincher");
+
+        public void intake(double power){}
+    }
+
 }
