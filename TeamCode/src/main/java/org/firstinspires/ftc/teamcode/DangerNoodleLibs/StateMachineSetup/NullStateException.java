e394f289ab7f463473bbc649cf421fff162d1eea
principledPanda <anish.c.pandya@gmail.com>
2019-10-01 12:03:25 -0500

    State Machine Setup

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DangerNoodleLibs/StateMachineSetup/NullStateException.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.DangerNoodleLibs.StateMachineSetup;
+
+public class NullStateException extends Exception {
+    public NullStateException(String message){
+        super(message);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DangerNoodleLibs/StateMachineSetup/StateMachine.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.DangerNoodleLibs.StateMachineSetup;
+
+public class StateMachine {
+}
