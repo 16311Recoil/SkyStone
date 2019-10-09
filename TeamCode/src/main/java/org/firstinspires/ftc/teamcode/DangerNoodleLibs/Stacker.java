390d53891fc46e429b4f867c62fe51c29a4158c1
AdityaAngajala <aditya.angajala01@k12.leanderisd.org>
2019-10-09 16:59:26 -0500

    Tele-Op Methods for Stacker Class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DangerNoodleLibs/Stacker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DangerNoodleLibs/Stacker.java
@@ -68,13 +68,19 @@ public class Stacker {
     private DcMotor lr; //lift right
     private Servo gr; // gantry right
     private Servo gl; // gantry left
-    private CRServo armRotater;
+    private Servo armRotater;
     private Servo pincher;
+    private boolean changeX;
+    private boolean changeY;
+    private boolean changeX2;
+    private boolean changeY2;
     private final double TIME_FOR_INTAKE = 3;
     private final double ENCODER_LIFT_UP = 2;  //TODO: Test times for Lift Times
     private final double ENCODER_LIFT_DOWN = 1.5;
     private final double CLOSED_PINCHER_SERVO_POSITION = 0; //TODO: Test for Servo Positions
     private final double OPEN_PICHER_SERVO_POSITION = 90;
+    private final double ARM_IN = 0;
+    private final double ARM_OUT = 1;
     private final double INCHES_TO_SERVO = 0;//TODO: Test conversions for inches in gantry movement to servo position
 
     public Stacker(LinearOpMode opMode) {
@@ -82,8 +88,7 @@ public class Stacker {
         ir = this.opMode.hardwareMap.dcMotor.get("ir");
         ll = this.opMode.hardwareMap.dcMotor.get("ll");
         lr = this.opMode.hardwareMap.dcMotor.get("lr");
-        armRotater = this.opMode.hardwareMap.crservo.get("armRotater");
-        armRotater.setDirection(DcMotorSimple.Direction.FORWARD);
+        armRotater = this.opMode.hardwareMap.servo.get("armRotater");
         pincher = this.opMode.hardwareMap.servo.get("pincher");
         gl = this.opMode.hardwareMap.servo.get("gl");
         gr = this.opMode.hardwareMap.servo.get("gr");
@@ -118,13 +123,14 @@ public class Stacker {
         }
     }
 
-    public void rotateArmTime(double power, double time) {
-        ElapsedTime timer = new ElapsedTime();
-        while (timer.seconds() < time) {
-            armRotater.setPower(power);
-        }
-        armRotater.setPower(0);
 
+    public void setArmPosition(boolean in) {
+        if (in){
+            armRotater.setPosition(ARM_IN);
+        }
+        else{
+            armRotater.setPosition(ARM_OUT);
+        }
     }
 
     public void setLiftPosition(double power, boolean up) {
@@ -162,5 +168,72 @@ public class Stacker {
     public double inchToServo(double inches){
         return (inches * INCHES_TO_SERVO);
     }
+    ////////////////////////////////////////////////////////////////////////////////////
+    //          Tele-Op
+    public void teleLiftTrigger(){
+        if ((opMode.gamepad1.right_trigger == 0) && (opMode.gamepad1.left_trigger == 0))
+            setLiftPower(0);
+        if (opMode.gamepad1.right_trigger != 0) {
+            setLiftPower(opMode.gamepad1.right_trigger);
+        }
+        else if (opMode.gamepad1.left_trigger != 0);{
+            setLiftPower(opMode.gamepad1.left_trigger);
+        }
+    }
+    public void teleLiftBumpers(double power){
+        if (opMode.gamepad1.right_bumper || opMode.gamepad2.right_bumper){
+            setLiftPower(power);
+        }
+        else if (opMode.gamepad1.left_bumper || opMode.gamepad2.left_bumper){
+            setLiftPower(-power);
+        }
+        else {
+            setLiftPower(0);
+        }
+    }
+    public void teleIntake(double power){
+        if (opMode.gamepad1.a || opMode.gamepad2.a){
+            setIntakePower(power);
+        }
+        else if (opMode.gamepad1.b || opMode.gamepad2.b){
+            setIntakePower(-power);
+        }
+        else {
+            setIntakePower(0);
+        }
+    }
+    public void telePincher(){
+        double counter = 0;
+        boolean close;
+        if ((opMode.gamepad1.x ^ changeX) || (opMode.gamepad2.x ^ changeX2)){
+            counter ++;
+        }
+        if ((counter % 2) == 0){
+            close = true;
+        }
+        else{
+            close = false;
+        }
+        setPincherPosition(close);
+        changeX = opMode.gamepad1.x;
+        changeX2 = opMode.gamepad2.x;
+    }
+    public void teleArm(){
+        double counter = 0;
+        boolean in;
+        if ((opMode.gamepad1.y ^ changeY) || (opMode.gamepad2.y ^ changeY2)){
+            counter ++;
+        }
+        if ((counter % 2) == 0){
+            in = true;
+        }
+        else{
+            in = false;
+        }
+        setArmPosition(in);
+        changeY = opMode.gamepad1.y;
+        changeY2 = opMode.gamepad2.y;
+    }
+
 }
 
