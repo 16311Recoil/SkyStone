99f79a5ea90e5062db6d8c0a957dd34604603425
DESKTOP-54OIDOM\sunid <sunidhi.ayyagari02@k12.leanderisd.org>
2020-08-12 14:12:28 -0500

    no message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAuto/Mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAuto/Mecanum.java
@@ -1,4 +1,43 @@
 package org.firstinspires.ftc.teamcode.BasicAuto;
 
-public class Mecanum {
-}
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.DangerNoodle.TeleOP;
+
+public class Mecanum extends OpMode {
+    public DcMotor RF;
+    public DcMotor LF;
+    public DcMotor RB;
+    public DcMotor LB;
+
+
+    public void init(){
+        RF = hardwareMap.dcMotor.get("RF");
+        RB = hardwareMap.dcMotor.get("RB");
+        LF = hardwareMap.dcMotor.get("LF");
+        LB = hardwareMap.dcMotor.get("LB");
+
+        RF.setDirection(DcMotor.Direction.REVERSE);
+        RB.setDirection(DcMotor.Direction.REVERSE);
+
+    }
+    public void loop(){
+        double vertical;
+        double horizontal;
+        double turn;
+
+        while (){
+            vertical = -gamepad1.left_stick_y;
+            horizontal = gamepad1.left_stick_x;
+            turn = gamepad1.right_stick_x;
+
+            RF.setPower(-turn+(vertical-horizontal));
+            RB.setPower(-turn+(vertical+horizontal));
+            LF.setPower(turn+(vertical+horizontal));
+            LB.setPower(turn+(vertical-horizontal));
+        }
+
+    }
+
+    }
